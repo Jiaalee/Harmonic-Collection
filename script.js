20a92018afeac04f88bb4aad22c20592652f8d57
leejia0704 <145470836+Jiaalee@users.noreply.github.com>
2023-12-05 05:31:03 -0500

    finall

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 function changeImage() {
-    var images = ["img/Group 83 (1).svg", "img/Group 84 (1).svg", "img/Group 85.svg", "img/Group 86.svg", "img/Group 87.svg"]; // Array of image paths
+    var images = ["img/Group 83 (1).svg", "img/Group 84 (1).svg", "img/Group 85.svg", "img/Group 86.svg", "img/Group 87.svg"];
     var imgElement = document.getElementById('clickableImage');
-    var currentIndex = images.indexOf(imgElement.src); // Get the index of the current image
+    var currentIndex = images.indexOf(imgElement.src);
   
-    // Get a random index that is not the current index
     var randomIndex = currentIndex;
     while (randomIndex === currentIndex) {
       randomIndex = Math.floor(Math.random() * images.length);
     }
   
-    imgElement.src = images[randomIndex]; // Change the image source to the randomly selected image
+    imgElement.src = images[randomIndex];
   }
   
+  function changeImage2() {
+    var images = ["img/Group 73 copy 2.svg", "img/Group 74 copy.svg"];
+    var imgElement = document.getElementById('clickableImage2');
+    var currentIndex = images.indexOf(imgElement.src);
+  
+    var randomIndex = currentIndex;
+    while (randomIndex === currentIndex) {
+      randomIndex = Math.floor(Math.random() * images.length);
+    }
+  
+    imgElement.src = images[randomIndex];
+    
+    var container = document.getElementById('imageContainer');
+    container.style.display = 'flex';
+    container.style.justifyContent = 'center';
+    container.style.alignItems = 'center';
+
+  imgElement.style.width = '350px'; // Set the width to 150 pixels
+  imgElement.style.height = '800px'; // Set the height to 150 pixels
+
+  }
+
